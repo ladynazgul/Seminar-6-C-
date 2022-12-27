cb95fc3b8535f67300d0eb9c8983d88e90484204
ladynazgool <ladynazgool@gmail.com>
2022-12-27 14:19:41 +0100

    Task-48 beginning

--- Task-01_48/Program.cs
+++ Task-01_48/Program.cs
@@ -1,6 +1,22 @@
 ﻿// Показать двумерный массив размером m×n заполненный целыми числами
 
-int[] CreateMatrix(uint rows, uint columns)
+int[,] CreateMatrix(int row, int col, int min, int max)
 {
-    
+    int[,] table = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.Length(0); i++)
+    {
+        for (int j = 0; i < matr.Length(0); i++)
+    }
 }
