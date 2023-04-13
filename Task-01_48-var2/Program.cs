842f430d2ade4b1f722101ee79c5b09613c90b75
ladynazgool <ladynazgool@gmail.com>
2023-04-13 08:27:59 +0200

    var2 added

---
+++ Task-01_48-var2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами, это то, что показал Олег
+
+int[,] CreateMatrix(uint rows, uint columns, int start, int end) // uint - означает неотрицательные числа
+{
+    Random random = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            matrix[i, j] = random.Next(start, end);
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(9, 9, 0, 20);
+PrintMatrix(matrix);
