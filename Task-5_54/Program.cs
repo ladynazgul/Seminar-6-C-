900de1b5d120d6d33969885ca89e062d69009682
ladynazgool <ladynazgool@gmail.com>
2023-04-19 13:42:25 +0200

    Task 54 finished

---
+++ Task-5_54/Program.cs
@@ -0,0 +1,63 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+int[,] CreateMantrix(int row, int col, int min, int max)
+{
+    int[,] arr = new int[row, col];
+    for (int i =0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 10) Console.Write($" {array[i, j]}  ");
+            else Console.Write($"  {array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetAverage(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("----------------------");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+
+int row = 5;
+int col = 5;
+int min = 0;
+int max = 20;
+
+int[,] matrix = CreateMantrix(row, col, min, max);
+PrintMatrix(matrix);
+double[] average = GetAverage(matrix);
+PrintArray(average);
+Console.WriteLine();
+Console.WriteLine();
---
+++ Task-5_54_var2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов
+
+void FillArray(int [,] array, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetAverage(int[,] matrix)
+{
+    double[] average = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        average[j] = Math.Round((Convert.ToDouble(sum) / Convert.ToDouble(matrix.GetLength(1))), 1);
+    }
+    return average;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($" # {i + 1} = {array[i]}  ");
+    }
+}
+
+Console.Write(" Введите количество строк ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.Write(" Введите количество столбцов ");
+int y = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int [x, y];
+FillArray(matrix, 0, 20);
+PrintMatrix(matrix);
+double[] average = GetAverage(matrix);
+Console.WriteLine("Среднее арифметическое равно: ");
+PrintArray(average);
