658f61b3b21bc901c0ffca76965ed11cddf9f4ff
ladynazgool <ladynazgool@gmail.com>
2023-04-19 07:47:33 +0200

    Task 53 finished new variant added

--- Task-4_53/Program.cs
+++ Task-4_53/Program.cs
@@ -36,9 +36,9 @@ int[] FindPosition(int[,] matr, int number)
 void PrintMatrix(int[,] matrix)
 {
     Console.WriteLine("Массив:");
-    for (int i = 0; i < matrix.GetLength; i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength; j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             if (matrix[i, j] / 1000 > 0) Console.Write($"{matrix[i, j]}  ");
             else if (matrix[i, j] / 100 > 0) Console.Write($" {matrix[i, j]}  ");
@@ -48,3 +48,31 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
+void ShowPosition(int[] pos, int num)
+{
+    Console.WriteLine();
+    if (pos[0] > 0 && pos[1] > 0) Console.WriteLine($"Число {num} есть в данном архиве. Находится в {pos[0]}-й строке, {pos[1]}-м столбце");
+    else Console.WriteLine($"Числа {num} в данном архиве нет");
+    Console.WriteLine();
+}
+
+int SetNumber()
+{
+    Console.WriteLine();
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int row = 5;
+int col = 7;
+int min = 10;
+int max = 100;
+
+int[,] mainMatrix = CreateMatrix(row, col, min, max);
+PrintMatrix(mainMatrix);
+int number = SetNumber();
+int[] position = FindPosition(mainMatrix, number);
+ShowPosition(position, number);
+
