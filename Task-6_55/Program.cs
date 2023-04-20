6012d90eba4e6a3701cb9d9fab368bf8f23410ca
ladynazgool <ladynazgool@gmail.com>
2023-04-20 10:59:26 +0200

    Task 56 finished

---
+++ Task-6_55/Program.cs
@@ -0,0 +1,49 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] CreateMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] ReplaceRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int value = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = value;
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 10) Console.Write($" {array[i, j]}  ");
+            else Console.Write($"  {array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 3;
+int col = 5;
+int min = 1;
+int max = 20;
+
+int[,] matrix = CreateMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] newMatrix = ReplaceRow(matrix);
+PrintMatrix(newMatrix);
---
+++ Task-7_56/Program.cs
@@ -0,0 +1,59 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int[,] FillMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int FindRow(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+        result = sum;
+        index = i;
+        }
+    }
+    return index;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 7;
+int min = 1;
+int max = 10;
+
+int[,] matrix = FillMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine("***********");
+int index = FindRow(matrix);
+Console.WriteLine($"Наименьшая сумма элементов в строке с индексом {index}");
